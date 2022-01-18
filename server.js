91a8e0e25583a91f0337e6fed4d716fa657e5894
IMRANDIL <aliimranadil2@gmail.com>
2022-01-18 18:58:57 +0530

    yh

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on Port: ${PORT}`);
+})
